70c69cfdcdb222da5cf797de9df0eca1361701f4
jyoti2y5 <jyotiyadav46408@gmail.com>
2023-02-09 00:09:36 +0530

    check player option

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -7,5 +7,16 @@ public class SnakeAndLadder {
         System.out.println("Single player Start position is " +StartPosition);
         System.out.println("Roll dice number is " +CheckNumber);
 
+        int FindPlayOption =(int)(Math.random() * 2);
+        if (FindPlayOption==0){
+            System.out.println("player no move stay in the same becouse " +FindPlayOption +" position");
+        }
+        else if (FindPlayOption==1){
+            System.out.println("player move ahead because " +FindPlayOption +" position");
+        }
+        else{
+            System.out.println("player moves behind because " +FindPlayOption +" position");
+        }
+
     }
 }
