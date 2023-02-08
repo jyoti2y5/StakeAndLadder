8bd01402b0c10b6cad50e1b360f024dda8c2119f
jyoti2y5 <jyotiyadav46408@gmail.com>
2023-02-08 21:20:47 +0530

    UC1

---
+++ SnakeAndLadder.java
@@ -0,0 +1,10 @@
+package com.sl.game;
+
+public class SnakeAndLadder {
+    public static void main(String[] args) {
+        int StartPosition =0;
+        System.out.println("Single player Start position is " +StartPosition);
+
+
+    }
+}
