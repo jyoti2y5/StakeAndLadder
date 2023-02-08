72d32d6f9e1a0e425aa734a6adeba4e5c54bbbe3
jyoti2y5 <jyotiyadav46408@gmail.com>
2023-02-08 23:44:03 +0530

    random dise number

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -3,8 +3,9 @@ package com.sl.game;
 public class SnakeAndLadder {
     public static void main(String[] args) {
         int StartPosition =0;
+        int CheckNumber = (int) (Math.random()*6) +1;
         System.out.println("Single player Start position is " +StartPosition);
-
+        System.out.println("Roll dice number is " +CheckNumber);
 
     }
 }
